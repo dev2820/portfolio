3dcda2156cef862b341348a8e3111a645de6ad9e
dev2820 <second28200@gmail.com>
2022-03-23 19:01:39 +0900

    [수정] GNB header에서 사용하는 경우 스토리를 작성함

--- src/components/GlobalNavigationBar/GlobalNavigationBar.stories.ts
+++ src/components/GlobalNavigationBar/GlobalNavigationBar.stories.ts
@@ -1,5 +1,6 @@
 import GlobalNavigationBar from './GlobalNavigationBar.vue';
-
+import {anchor} from './GlobalNavigationBar.mock.json';
+import IAnchor from '@/interfaces/IAnchor';
 export default {
   component: GlobalNavigationBar,
   title: 'GNB/GlobalNavigationBar',
@@ -7,5 +8,26 @@ export default {
 
 export const Default = (args:any) => ({
   components: { GlobalNavigationBar },
-  template: '<GlobalNavigationBar>클릭</GlobalNavigationBar>',
+  setup() {
+    const anchors:IAnchor[] = anchor;
+    return {anchors};
+  },
+  template: '<GlobalNavigationBar :anchors="anchors">클릭</GlobalNavigationBar>',
+});
+
+export const Header = (args:any) => ({
+  components: { GlobalNavigationBar },
+  setup() {
+    const anchors:IAnchor[] = anchor;
+    const headerStyle = {
+      width:"100%",
+      textAlign:"center",
+      background:"var(--glass-background)"
+    }
+    return {anchors,headerStyle};
+  },
+  template: `
+  <header :style="headerStyle">
+    <GlobalNavigationBar :anchors="anchors">클릭</GlobalNavigationBar>
+  </header>`,
 });
