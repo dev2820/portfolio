cf1e0b409d12a9a5b4d8b986ebf5524491258b15
dev2820 <second28200@gmail.com>
2022-03-23 19:02:13 +0900

    [수정] 다크모드 적용

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,15 @@
 <template id="app">
+  <span class="darkmode-toggle">
+    <darkmode-toggle class="darkmode-toggle" :checked="isOsDarkmode" @on="darkmodeHandler(true)" @off="darkmodeHandler(false)"/>
+  </span>
   <header>
     <global-navigation-bar :anchors="anchor"/>
   </header>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
-  <p>what</p>
+  <main>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+    <p>what</p>
+  </main>
 </template>
 
 <script lang="ts">
@@ -12,19 +17,33 @@ import { defineComponent } from "vue";
 import { anchor } from "@/assets/meta.json"
 import HelloWorld from "@/components/HelloWorld.vue";
 import GlobalNavigationBar from "./components/GlobalNavigationBar/GlobalNavigationBar.vue";
+import DarkmodeToggle from "./components/Toggle/DarkmodeToggle.vue";
 export default defineComponent({
   components: {
     HelloWorld,
     GlobalNavigationBar,
+    DarkmodeToggle
   },
   setup() {
+    const darkmodeHandler = (isOn:boolean):void => {
+      if(isOn) {
+        document.documentElement.setAttribute('color-mode','dark');
+      }
+      else {
+        document.documentElement.setAttribute('color-mode','light');
+      }
+    }
+    const isOsDarkmode:boolean = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
     return {
-      anchor
+      anchor,
+      darkmodeHandler,
+      isOsDarkmode
     }
   }
 });
 </script>
-<style>
+<style lang="scss">
+
 @import url("@/assets/css/color.css");
 @import url("@/assets/css/font.css");
 @import url("@/assets/css/icon.css");
@@ -36,13 +55,30 @@ export default defineComponent({
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  background:var(--background);
+  color:(--on-background);
 }
 body {
   margin:0;
   padding:0;
+  header {
+    background:var(--glass-background);
+    position:fixed;
+    width:100%;
+    height:5rem;
+    display:flex;
+    justify-content: center;
+  }
+  main {
+    padding-top:100px;
+  }
 }
-header {
-  background:var(--glass-background);
+.darkmode-toggle{
+  position:fixed;
+  right:1rem;
+  bottom:1rem;
+  margin-top:2rem;
 }
+
+
 </style>
