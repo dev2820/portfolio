e61718d30749a05f9f596f1d54c138e56d66b585
dev2820 <second28200@gmail.com>
2022-03-27 17:19:13 +0900

    [추가] 방향 버튼역할을 할 ArrowButton 컴포넌트 추가

---
+++ src/components/Button/ArrowButton.stories.ts
@@ -0,0 +1,30 @@
+import ArrowButton from './ArrowButton.vue';
+
+export default {
+  component: ArrowButton,
+  title: 'Button/ArrowButton',
+};
+
+// const Template = (args:any) => ({
+//     components: { ArrowButton },
+//     setup() {
+//         const type:string = args;
+//         return {
+//           type
+//         };
+//     },
+//     template: '<ArrowButton>클릭</ArrowButton>',
+// });
+
+// export const Default = Template.bind({});
+// Default({});
+
+export const Default = (args:any) => ({
+    components: { ArrowButton },
+    template: '<ArrowButton>클릭</ArrowButton>',
+});
+
+export const Primary = (args:any) => ({
+  components: { ArrowButton },
+  template: '<ArrowButton primary>클릭</ArrowButton>',
+});
---
+++ src/components/Button/ArrowButton.vue
@@ -0,0 +1,75 @@
+<template>
+  <button>
+    <img :style="directStyle" :src="arrowSrc" />
+  </button>
+</template>
+
+<script lang="ts">
+import { ref, defineComponent, computed, reactive, toRef } from "vue";
+import arrowLight from "@/assets/png/arrow-big-top-light.png"
+import arrowDark from "@/assets/png/arrow-big-top-dark.png"
+type directionType = 'top'|'botton'|'right'|'left'
+export default defineComponent({
+  name: "ArrowButton",
+  props: {
+    direction:{
+      type:String as () => directionType,
+      default() {
+        return 'top';
+      }
+    },
+    isDarkmode: {
+      type:Boolean,
+      default() {
+        return false;
+      }
+    }
+  },
+  setup(props) {
+    props = reactive(props);
+    const directionRef = toRef(props, "direction");
+    const isDarkmodeRef = toRef(props,'isDarkmode');
+
+    const directStyle = computed(()=>{
+      let degree = 0;
+      if(directionRef.value === 'top') degree = 0;
+      else if(directionRef.value === 'bottom') degree = 180;
+      else if(directionRef.value === 'right') degree = 90;
+      else if(directionRef.value === 'left') degree = 270;
+      return {
+        'transform':`rotate(${degree}deg)`
+      }
+    })
+
+    const arrowSrc = computed(()=> {
+      if(isDarkmodeRef.value) {
+        return arrowDark;
+      }
+      else {
+        return arrowLight;
+      }
+    })
+    return {
+      directStyle,
+      arrowSrc
+    }
+  },
+});
+</script>
+
+<style scoped lang="scss">
+button {
+  border: none;
+  padding: 0.5rem 2rem;
+  border-radius: 50%;
+  background-color: var(--button-background);
+  filter: var(--drop-shadow);
+  box-shadow: var(--button-inner-shadow);
+  cursor: pointer;
+  
+  img {
+    width:72px;
+    height:72px;
+  }
+}
+</style>
