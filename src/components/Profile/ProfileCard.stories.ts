80de5ec0c9c0d4612f8be6c0da198be3da9c2505
dev2820 <second28200@gmail.com>
2022-03-28 16:01:53 +0900

    [추가] 프로필 카드 컴포넌트 생성

---
+++ src/components/Profile/ProfileCard.stories.ts
@@ -0,0 +1,23 @@
+import ProfileCard from './ProfileCard.vue';
+import { action } from "@storybook/addon-actions"
+import dummy from "@/assets/dummy_profile.png"
+export default {
+  component: ProfileCard,
+  title: 'Profile/ProfileCard',
+};
+
+export const Default = (args:any) => ({
+    components: { ProfileCard },
+    setup() {
+        const profile = dummy;
+        return {
+          profile
+        };
+    },
+    template: '<ProfileCard :profile="profile"/>',
+});
+
+export const NoProfile = (args:any) => ({
+  components: { ProfileCard },
+  template: '<ProfileCard/>',
+});
---
+++ src/components/Profile/ProfileCard.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="profile">
+    <div class="background"></div>
+    <img @error="noImage($event)" :src="profile"/>
+  </div>
+</template>
+
+<script lang="ts">
+import { ref, defineComponent, computed, reactive, toRef } from "vue";
+import noProfile from "@/assets/noProfile.png"
+export default defineComponent({
+  name: "ProfileCard",
+  props: {
+    profile: {
+      type: String,
+      default: "",
+    },
+  },
+  emits: ["on", "off"],
+  setup(props, { emit }) {
+    props = reactive(props);
+    const noImage = (evt:Event):void => {
+      const target = evt.target as HTMLImageElement
+      target.src = noProfile;
+    }
+    return {
+      noImage
+    };
+  },
+});
+</script>
+
+<style scoped lang="scss">
+div.profile {
+  width:448px;
+  height:548px;
+  display:inline-block;
+  & > * {
+    position:absolute;
+    border-radius: 1rem;
+  }
+  img {
+    left:0;
+    top:0;
+    width:400px;
+    height:500px;
+    filter:sepia(60%);
+    box-shadow:var(--card-box-shadow);
+  }
+  div.background {
+    width:400px;
+    height:500px;
+    left:48px;
+    top:48px;
+    background:var(--primary-500);
+  }
+
+}
+</style>
